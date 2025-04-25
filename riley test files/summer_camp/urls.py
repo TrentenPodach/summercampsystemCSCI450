a1c3827a342f4238cf36f159ef44295273484757
Trenten Podach <trentenpodach@outlook.com>
2025-04-25 17:07:19 -0400

    Added layout.html, static, and django-bootstrap
    Added layout.html, which can be used as a basic layout for html pages and includes a universal navigation bar. Added static, which allows for the use of images, css, and javascript for html pages. Also added django-bootstrap to requirements.txt and adjusted backports.zoneinfo (its only needed for older versions of python apparently)

--- riley test files/summer_camp/urls.py
+++ riley test files/summer_camp/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('campreg.urls')),  # Include your app’s URLs
 ]
+
+urlpatterns += staticfiles_urlpatterns()
