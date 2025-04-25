b2531eda05878cc661124c48a4e4466a1f7122ec
Trenten Podach <trentenpodach@outlook.com>
2025-04-25 12:41:45 -0400

    Created new branch
    Created new branch to test a Django guide I found :)

--- riley test files/campreg/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Individual, Family, Account, Camp, WaitingList, MailingList
-
-admin.site.register(Individual)
-admin.site.register(Family)
-admin.site.register(Account)
-admin.site.register(Camp)
-admin.site.register(WaitingList)
-admin.site.register(MailingList)
--- riley test files/campreg/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class CampregConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'campreg'
