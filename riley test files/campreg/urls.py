77fb7a25222939538efa565ed2004f7501d29e8f
Riley Pence <rileypence4@gmail.com>
2025-05-03 14:25:40 -0700

    Added root URL path to direct '/' to the home view for landing page access

--- riley test files/campreg/urls.py
+++ riley test files/campreg/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.home, name='root'),  # 🆕 Root path
     path('register/', views.register, name='register'),
     path('home/', views.home, name='home'),
     path('success/', views.registration_success, name='registration_success'),
